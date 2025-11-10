4a8fb7e307ae7864a7d75e45c0be3f7f2b3b86a8
Anuchoudhary00 <anjuc5448@gmail.com>
2025-11-10 23:47:07 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,130 @@
+<?php include 'connection.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <h2> Content Form</h2><br>
+
+        <form action="#" method="POST">
+            <div class="form">
+
+                <div class="input_field">
+                    <label for="fname">First Name:</label>
+                    <input type="text" id="fname" name="fname" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="lname">Last Name:</label>
+                    <input type="text" id="lname" name="lname" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="phone">Phone Number:</label>
+                    <input type="text" id="phone" name="phone" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="age">Age:</label>
+                    <input type="text" id="age" name="age" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="age">Gender:</label>
+                    <select name="gender">
+                        <option>Select</option>
+                        <option>Male</option>
+                        <option>Female</option>
+                    </select>
+                </div>
+                <br>
+
+
+                <div class="input_field">
+                    <label for="occupation">Occupation:</label>
+                    <input type="text" id="occupation" name="occupation" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="address">Address: </label>
+                    <input type="text" id="address" name="address" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="city">City: </label>
+                    <input type="text" id="city" name="city" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="code">Zip Code: </label>
+                    <input type="text" id="code" name="code" required class="input">
+                </div>
+                <br>
+
+                <div class="input_field">
+                    <label for="country">Country: </label>
+                    <input type="text" id="country" name="country" required class="input">
+                </div>
+                <br>
+
+                <div class="btn1">
+                    <input type="submit" value="Submit" name="submit" class="btn">
+                </div>
+        </form>
+
+    </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</body>
+
+</html>
+
+
+<?php
+if (isset($_POST['submit'])) {
+    $fname       = $_POST['fname'];
+    $lname       = $_POST['lname'];
+    $email       = $_POST['email'];
+    $phone       = $_POST['phone'];
+    $age         = $_POST['age'];
+    $gender      = $_POST['gender'];
+    $occupation   = $_POST['occupation'];
+    $address      = $_POST['address'];
+    $city         = $_POST['city'];
+    $code          = $_POST['code'];
+    $country     = $_POST['country'];
+
+    $query = "INSERT INTO client_data1(First_Name, Last_Name, Email, Phone_Number, Age, Gender, Occupation, Address, City, ZipCode, Country)
+    
+     values('$fname', '$lname', '$email', '$phone', '$age', '$gender', '$occupation', '$address', '$city', '$code', '$country')";
+    $data = mysqli_query($conn, $query);
+
+    if ($data) {
+        echo "Data inserted into database";
+    } else {
+        echo "Failed to insert data into database";
+    }
+}
+?>
